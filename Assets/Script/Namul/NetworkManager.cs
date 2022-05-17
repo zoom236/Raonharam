a0d029b78a9847fa32be2f67a166148d19ef27ee
ParkYiJun <ydlwnsj25@gmail.com>
2022-05-17 11:13:52 +0900

    Temporary Sample Scene and Scripts

--- Assets/Script/Namul/NetworkManager.cs
+++ Assets/Script/Namul/NetworkManager.cs
@@ -46,7 +46,7 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     {
         DisconnectPanel.SetActive(false);
         Spawn();
-        main.SetActive(false);
+        //main?.SetActive(false);
     }
 
     IEnumerator DestroyBullet()
@@ -64,16 +64,16 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 
     public void Spawn()
     {
-        main.SetActive(false);
+        //main?.SetActive(false);
         if (!doke)
         {
-            PhotonNetwork.Instantiate("Player", SpawnPos.position, Quaternion.identity);
+            PhotonNetwork.Instantiate("Kid", SpawnPos.position, Quaternion.identity);
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
         }
         else
         {
-            PhotonNetwork.Instantiate("Enemy", SpawnPos.position, Quaternion.identity);
+            PhotonNetwork.Instantiate("Dokkebi", SpawnPos.position, Quaternion.identity);
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
         }
@@ -84,6 +84,6 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     {
         DisconnectPanel.SetActive(true);
         RespawnPanel.SetActive(false);
-        main.SetActive(true);
+        //main?.SetActive(true);
     }
 }
